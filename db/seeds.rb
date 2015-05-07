154da8a995e5a45c7aa47e015a02901e260e87e3
Sadiq Okocha <sokocha@gmail.com>
2015-05-07 15:55:01 +0100

    edits the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Question.delete_all
 
-Question.create(question: "What is the capital of Japan?", answer: "Tokyo")
-Question.create(question: "What is 70 ÷ 10 ?", answer: "7")
-Question.create(question: "What is the closest planet to the Sun?", answer: "Mercury")
+Question.create(question: "<kbd>How old is Sadiq Okocha? </kbd>", answer: '27')
+Question.create(question: "<pre><h4>What does H<sub>2</sub>O represent?</h4></pre>", answer: 'water')
+Question.create(question: "<h2><i><samp>Who let the</samp> <code>dogs</code> out?</i></h2>", answer: 'snoopy')
+
+Question.create(question: 'if <var>x</var> = 3, what is  <code><b><var>x</var></b> + <b>4</b></code>?', answer: '7')
+Question.create(question: '<h4><kbd>How old</kbd> are you?</h4>', answer: '27')
+Question.create(question: 'Which bear is best? <ul><li>brown bear</li><li>panda bear</li><li>polar bear</li></ul>', answer: 'brown bear')
+Question.create(question: 'What color <kbd>is <code>white</code> wine</kbd>?', answer: 'wine')
+Question.create(question: 'What comes next? <ol><li></li><li></li><li></li><li></li></ol>', answer: '5')
+Question.create(question: '<b>How</b> does the <i>kitty</i> cat go?', answer: 'meow')
+Question.create(question: '<pre>what would you do for a <strong>klondike</strong> bar?</pre>', answer: 'anything!')
+Question.create(question: "<samp>When</samp> was the <sub>following</sub> company founded <a href = 'http://google.com'>Google</a>?", answer: '1996')
